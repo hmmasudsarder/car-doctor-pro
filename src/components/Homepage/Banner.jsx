21473334c3ba7501cf63e664c2ce52ec124c18e3
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-09-21 19:10:44 +0600

    banner section adde finits

--- src/components/Homepage/Banner.jsx
+++ src/components/Homepage/Banner.jsx
@@ -1,11 +1,12 @@
 const Banner = () => {
   return (
-    <div>
-      <div className="carousel w-full mt-[5.5rem]">
+    <div className="container mx-auto">
+      <div className="carousel w-full mt-16">
         {banners.map((banner, inx) => (
-          <div style={{backgroundImage: `linear-gradient(45deg,rgba(7, 25, 82, 0.7), rgba(0, 0, 0, 0.3)), url(/assets/images/banner/${inx+1}.jpg)`}} key={inx} id={`slide${inx+1}`} className="carousel-item h-[90vh] bg-top bg-no-repeat bg-cover relative w-full">
-            <div className="">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In nam error dolores voluptatum officiis? Enim doloribus numquam maxime, praesentium ea inventore.
+          <div style={{backgroundImage: `linear-gradient(45deg,rgba(7, 25, 82, 0.7), rgba(0, 0, 0, 0.3)), url(/assets/images/banner/${inx+1}.jpg)`}} key={inx} id={`slide${inx+1}`} className="carousel-item h-[90vh] bg-top bg-no-repeat bg-cover relative w-full rounded-lg">
+            <div className="w-full h-full flex items-center pl-32">
+               <h2 className="text-5xl font-bold text-white">{banner.title}</h2>
+               <p className="text-white">{banner.description}</p>
             </div>
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
               <a href={banner.prev} className="btn btn-circle">
